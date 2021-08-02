56a120cd44fd71fe1a63e52172fdb9cf11aae2f6
ShySame <67509397+ShySame@users.noreply.github.com>
2021-08-02 16:45:38 +0300

    useri

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from faker import Faker
 
 app = Flask(__name__)
 
@@ -6,6 +7,24 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+# -------------------------------------------------------------------------------------
+@app.route('/generate-users/')
+def users():
+    kol=int(request.args.get("numbers"))
+    print(kol)
+    fake = Faker()
+    user = []
+    for i in range(kol):
+        name = fake.name()
+        email = "".join(name.split()) + "@mail.com"
+        user_ = name + " : " + email.lower()
+        user.append(user_)
+    print(user)
+
+    return render_template("user_gen.html", us=user, kol=kol)
+# -------------------------------------------------------------------------------------
 
 
 if __name__ == '__main__':
